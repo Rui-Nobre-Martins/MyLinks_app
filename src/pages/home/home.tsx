8c2e73ecfe4c6f816740b496ef234bee770af153
Rui  Martins <>
2025-07-23 19:06:41 +0100

    Homepage finished, started login page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,51 @@
+import { Social } from "../../components/social/social"
 
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa6"
 
 export function Home(){
     return(
         <>
-        <h1>
-            Página Home
-        </h1>
+        <div className="w-full flex flex-col justify-center items-center py-4">
+            <h1 className="md:text-4xl text-white text-3xl font-bold mt-20">
+                My Links App
+            </h1>
+            <span className="text-green-50 mb-5 mt-4">
+                👇 See all my links 👇
+            </span>
+
+            <main className="flex flex-col w-11/12 max-w-xl text-center">
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-x-105 cursor-pointer">
+                    <a>
+                        <p className="md:text-lg text-base">
+                            LinkedIn
+                        </p>
+                    </a>
+                </section>
+
+                <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-x-105 cursor-pointer">
+                    <a>
+                        <p className="md:text-lg text-base">
+                            Git Hub
+                        </p>
+                    </a>
+                </section>
+
+                <footer className="flex justify-center gap-3 my-4">
+                    <Social url="https://www.linkedin.com/in/rui-nobre-martins-29217bba/">
+                        <FaLinkedin size={35} color="#fff"  className="transition-transform hover:scale-y-105"/>
+                    </Social>
+
+                    <Social url="https://github.com/Rui-Nobre-Martins/">
+                        <FaGithub size={35} color="#fff" className="transition-transform hover:scale-y-105"/>
+                    </Social>
+
+                    <Social url="https://www.instagram.com/ruinobremartins/">
+                        <FaInstagram size={35} color="#fff" className="transition-transform hover:scale-y-105"/>
+                    </Social>
+                </footer>
+            </main>
+        </div>
+        
         </>
     )
 }
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 
 export function Login(){
     return(
         <>
-        <h1>
-            Página Login
-        </h1>
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <h1>
+                <Link to="/">
+                    <h1 className="mt-11 text-white mb-7 font-bold text-5xl">My 
+                    <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent"> Links App</span>
+                    </h1>
+                </Link>
+            </h1>
+        </div>
         </>
     )
 }
