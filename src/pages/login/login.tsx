048a407c9f01eb9b2bd76be0dd328132591ed5aa
Rui  Martins <>
2025-07-23 20:54:01 +0100

    Login page created, input already working

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,20 @@
 import { Link } from "react-router-dom";
+import { Input } from "../../components/input/input";
+import { useState, type FormEvent } from "react";
 
 export function Login(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e: FormEvent){
+        e.preventDefault();
+        console.log({
+            email: email,
+            password: password
+        })
+    }
+
     return(
         <>
         <div className="w-full h-screen flex flex-col justify-center items-center">
@@ -10,7 +24,30 @@ export function Login(){
                     <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent"> Links App</span>
                     </h1>
                 </Link>
+
+                <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col px-2">
+                    <Input
+                        placeholder="Email"
+                        type="email"
+                        value={email}
+                        onChange={(e)=> setEmail(e.target.value)}
+                    />
+
+                    <Input
+                        placeholder="Password"
+                        type="password"
+                        value={password}
+                        onChange={(e)=> setPassword(e.target.value)}
+                    />          
+
+                    <button 
+                    type="submit"
+                    className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white cursor-pointer">
+                        Login
+                    </button>
+                </form>
             </h1>
+
         </div>
         </>
     )
